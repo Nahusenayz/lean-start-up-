1cb5384ab138fb0cb00ed396f149602be9edfaf0
nahuseny <itsnz333@gmail.com>
2025-11-05 16:23:17 +0300

    Make Start Reading open the chapter menu

--- App.tsx
+++ App.tsx
@@ -128,7 +128,6 @@ const App: React.FC = () => {
             onNext={handleNextChapter}
             hasPrevious={hasPrevious}
             hasNext={hasNext}
-            onOpenMenu={() => setIsSidebarOpen(true)}
           />
         </main>
       </div>
